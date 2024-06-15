29c624304c15138922aeb41148c5b778fc1a92b2
Jimmy  Vu <jimmyvu@d207-023-174-080.wireless.sfu.ca>
2024-06-14 18:56:14 -0700

    env setup for render 4

--- src/main/java/com/example/rectangledb/RectangledbApplication.java
+++ src/main/java/com/example/rectangledb/RectangledbApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.core.env.Environment;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class RectangledbApplication {
 
@@ -12,6 +14,12 @@ public class RectangledbApplication {
     private Environment env;
 
     public static void main(String[] args) {
+		// Load environment variables from .env file
+        Dotenv dotenv = Dotenv.load();
+
+        // Set system properties for Spring Boot to use
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
         SpringApplication.run(RectangledbApplication.class, args);
 		
     }
